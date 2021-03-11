d474133701b06c0e54fa9fee8e7339e1cb26bc83
Joey Baruch <joey.baruch@gmail.com>
2021-03-11 13:56:44 +0200

    DB: attempt enable ssl

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,33 +1,25 @@
-  
 const parse = require('pg-connection-string').parse;
-const fallback = require('../../database');
-let config = undefined;
-try {
-  config = parse(process.env.DATABASE_URL);
-} catch {
-  console.log('failed to parse PG DATABASE_URL. Using fallback database configs');
-}
+const config = parse(process.env.DATABASE_URL);
 
-module.exports = ({ env }) =>
-  config
-    ? {
-        defaultConnection: 'default',
-        connections: {
-          default: {
-            connector: 'bookshelf',
-            settings: {
-              client: 'postgres',
-              host: config.host,
-              port: config.port,
-              database: config.database,
-              username: config.user,
-              password: config.password,
-              schema: 'public',
-            },
-            options: {
-              ssl: false,
-            },
-          },
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
         },
-      }
-    : fallback({ env });
+      },
+      options: {
+        ssl: true,
+      },
+    },
+  },
+});
